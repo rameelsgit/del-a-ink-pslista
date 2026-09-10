bcf65ba6047f87678cfb6d865a9358917f636c85
Rameel Siddiqui <rameelsiddiqui23@gmail.com>
2026-09-10 14:06:05 +0200

    while loop för input av vara och pris

--- inkopslista.cs
+++ inkopslista.cs
@@ -2,3 +2,22 @@
 List<string> varor = [];
 List<int> pris = [];
 
+// meddelandet för menyn
+Console.WriteLine("Väkommen Till inköpslistan");
+// bool är lagt till false så att den körs tills den blir klar 
+bool klar = false;
+
+// while loopen som körs till det blir true 
+while (!klar)
+{
+    Console.WriteLine(" Skriv in din vara: ");
+    string vara = Console.ReadLine()!;
+
+    Console.WriteLine("Skriv in priset: ");
+    int priset = int.Parse(Console.ReadLine()!);
+
+    varor.Add(vara);
+    pris.Add(priset);
+    
+    
+}
